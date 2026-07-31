e68419b80082ce3478882e5903116edbc2cb29c7
Smorgg <ka167954@gmail.com>
2026-07-31 13:44:12 +0900

    first commit

---
+++ skala-vue/src/components/practices/basic/SampleTwo.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const welcomeMassage = 'Welcome to Skala-Vue'
+</script>
+
+<template>
+    <div class="practice-section">
+        <h2>{{ welcomeMassage }}</h2>
+        <p>{{ welcomeMassage.toUpperCase() }}</p>
+        <p>{{ 'Random number: ' + Math.ceil(Math.random() * 100) }}</p>
+    </div>
+</template>
