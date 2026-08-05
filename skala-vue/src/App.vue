8e639e866359cf4b914cbe08f57c4da277c48e41
Smorgg <ka167954@gmail.com>
2026-08-05 12:39:06 +0900

    feat: 과제 4-1 UI 라이브러리 적용

--- skala-vue/src/App.vue
+++ skala-vue/src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
-import UiLibrary from './components/practices/library/UiLibrary.vue'
+import { RouterLink, RouterView } from 'vue-router'
+import UnitToggler from './components/exercise/UnitToggler.vue'
 </script>
 
 <template>
-  <UiLibrary />
+  <div class="app-container">
+    <div class="dashboard-wrapper">
+      <h1>종합실습 5: 스토어 적용</h1>
+      <nav class="navigation-bar">
+        <RouterLink to="/">날씨 대시보드</RouterLink>
+        <span class="divider">|</span>
+        <RouterLink to="/about">서비스 소개</RouterLink>
+        <UnitToggler></UnitToggler>
+      </nav>
+    </div>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <style>
